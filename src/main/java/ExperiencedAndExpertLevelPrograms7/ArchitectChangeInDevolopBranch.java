540d7d5bd2824b3dd7a5bd19721c1c826afddfb0
shiva24 <Shivashanker.Dorepally@valuemomentum.com>
2023-01-02 09:39:18 +0530

    User two done some changes for architect

---
+++ src/main/java/ExperiencedAndExpertLevelPrograms7/ArchitectChangeInDevolopBranch.java
@@ -0,0 +1,8 @@
+package ExperiencedAndExpertLevelPrograms7;
+
+public class ArchitectChangeInDevolopBranch {
+	public static void main(String[] args) {
+		System.out.println("User Two has changed architect changes to contribute");
+	}
+
+}
