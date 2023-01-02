8f92df2717444ca76e2dedd05d8357581fb9be57
shiva24 <Shivashanker.Dorepally@valuemomentum.com>
2023-01-02 13:24:27 +0530

    Architect has added change comments to _3ChildClassMethodHidding

--- src/main/java/ExperiencedAndExpertLevelPrograms7/_3ChildClassMethodHidding.java
+++ src/main/java/ExperiencedAndExpertLevelPrograms7/_3ChildClassMethodHidding.java
@@ -5,6 +5,9 @@ public class _3ChildClassMethodHidding extends _3ParentClassMethodHidding {
 	public static void staticOverrindMeansMethodHidding()
 	{
 		System.out.println("child mehod is called: staticOverrindMeansMethodHidding");
+		System.out.println("Architect Change 1");
+		System.out.println("Architect Change 2");
+		System.out.println("Architect Change 2");
 	}
 	
 	public static void main(String[] args) {
